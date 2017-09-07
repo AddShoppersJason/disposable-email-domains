a270ac76cac0bb73be98b66e61d9baafcfdb0f6c
Jason Ehmke <jason@addshoppers.com>
2017-09-07 10:00:25 -0400

    wrapped domain check in function

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+var domains = require('disposable-email-domains');
 
-/**
- * Expose the disposable domains.
- */
 
-module.exports = require('./index.json');
+const DomainCheck = {
+
+  checkDomain: function (domainToCheck, cb) {
+
+    var hasMatch = false;
+
+    for (var index = 0; index < domains.length; ++index) {
+
+      const domain = domains[index];
+      console.log('looping')
+
+      if (domain.toLowerCase() === domainToCheck.toLowerCase()) {
+        hasMatch = true;
+        break;
+      }
+    }
+
+    cb(null, hasMatch)
+
+  },
+};
+
+module.exports = DomainCheck;
